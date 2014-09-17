f04dc7e8ffa311ea620a0169985622db5b2848da
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-09-17 13:29:52 +0900

    .gitignore is now working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "登録成功♬"
+      redirect_to @user
     else
       render 'new'
     end

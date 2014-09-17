1b922e4c4c61f96f77d301973f25723958ff02e9
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-09-17 11:55:29 +0900

    show and signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+   def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation, :introduction)
+  end
+
 end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -20,5 +21,6 @@ module Dreamspace
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.i18n.default_locale = :ja
   end
 end

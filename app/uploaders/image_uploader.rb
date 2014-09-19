ec39a424aca73b8ae6beec558d537000383a06a1
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-09-19 17:57:39 +0900

    gitignore

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -22,16 +22,20 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
  # 拡張子が同じでないとGIFをJPGとかにコンバートできないので、ファイル名を変更
-  def filename
-    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
-  end
+#  def filename
+#    super.chomp(File.extname(super)) + '.jpg' if original_filename.present?
+#  end
 
  # ファイル名は日本語が入ってくると嫌なので、下記のようにしてみてもいい。
  # 日付(20131001.jpgみたいなファイル名)で保存する
   def filename
-    time = Time.now
-    name = time.strftime('%Y%m%d%H%M%S') + '.jpg'
-    name.downcase
+     "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+ 
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 
   storage :fog

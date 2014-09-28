67f9e22d76492a779d36303b0d68340e6e1705f4
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-09-28 15:33:12 +0900

    model

--- app/models/designer_registrant.rb
+++ app/models/designer_registrant.rb
@@ -1,2 +1,8 @@
 class DesignerRegistrant < ActiveRecord::Base
+  default_scope -> { order('created_at DESC') } 
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :specialty, presence: true
+  validates :prefecture, presence: true
+  validates :demand, presence:true
 end

1f6438a082d61efe36e548e1bab1ed5075bf28aa
Toshiaki-Ko <ko_toshiaki@nexway.co.jp>
2014-09-29 19:57:55 +0900

    time_zone

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Dreamspace
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
